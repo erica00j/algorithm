f889497d0a6c7c4d5304527aff4a918b54bde939
yuri <erica00j@naver.com>
2024-01-09 16:22:06 +0900

    ecote

---
+++ 이코테/곱하기혹은더하기.py
@@ -0,0 +1,12 @@
+def solution(N):
+    result = int(N[0])
+
+    for i in range(1,len(N)):
+        num = int(N[i])
+        if num<=1 or result<=1:
+            result+=num
+        else:
+            result*=num
+    return result
+
+print(solution('02984'))

9c9681c71ab539651b8d6bbab507d568653240f4
Yuri Chung <68888172+erica00j@users.noreply.github.com>
2024-06-26 13:57:41 +0900

    Update 바이러스.py

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -2,30 +2,28 @@ from collections import deque
 
 N = int(input())
 M  = int(input())
-#graph = [[1, 2], [2, 3], [1, 5], [5, 2], [5, 6], [4, 7]]
 
-# 각 노드의 연결 리스트를 저장할 graph_ 초기화 (0번 인덱스를 비우기 위해 max_node + 1)
-graph_ = [[] for _ in range(N + 1)]
+graph = [[] for _ in range(N + 1)]
 
 # 그래프 변환 작업
 for _ in range(M):
     node1, node2 = map(int, input().split())
-    graph_[node1].append(node2)
-    graph_[node2].append(node1)
+    graph[node1].append(node2)
+    graph[node2].append(node1)
 
-#graph_ = [[],[2, 5], [1, 3, 5], [2], [7], [1, 2, 6], [5], [4]]
+#graph = [[],[2, 5], [1, 3, 5], [2], [7], [1, 2, 6], [5], [4]]
 virus = []
 
-def bfs(graph_,start,visited):
+def bfs(graph,start,visited):
     queue = deque([start])
     visited[start] = True
     while queue:
         v = queue.popleft()
         virus.append(v)
-        for i in graph_[v]:
+        for i in graph[v]:
             if not visited[i]:
                 queue.append(i)
                 visited[i] = True
 visited = [False]*(N+1)
-bfs(graph_,1,visited)
+bfs(graph,1,visited)
 print(len(virus)-1)

21a64d172e9c87d2bc404a6a6692715d3e2d3950
Yuri Chung <68888172+erica00j@users.noreply.github.com>
2024-08-02 18:12:25 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -0,0 +1,27 @@
+N, M = map(int,input().split())
+graph = []
+answer = []
+for i in range(N):
+    graph.append(list(map(str,input())))
+
+for i in range(N-7):
+    for j in range(M-7):
+        count_1 = 0
+        count_2 = 0
+        
+        #8x8 크기로 자르기
+        for a in range(i,i+8):
+            for b in range(j,j+8):
+                if (a+b)%2 == 0:
+                    if graph[a][b] != 'B':
+                        count_1 += 1
+                    if graph[a][b] != 'W':
+                        count_2 += 1
+                else:
+                    if graph[a][b] != 'W':
+                        count_1 += 1
+                    if graph[a][b] != 'B':
+                        count_2 += 1
+        answer.append(count_1)
+        answer.append(count_2)
+print(min(answer))
